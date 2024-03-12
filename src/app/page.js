2814ab6b62ef8ff3859799c8417e9a948916f186
Cleef <cleefgulfan312@gmail.com>
2024-03-12 01:12:52 +0000

    Added logo to Navbar.

--- src/app/page.js
+++ src/app/page.js
@@ -20,7 +20,7 @@ export default function Home() {
           <Divider><p>Alternatively</p></Divider>
 
           <Label htmlFor="message" value='Copy & Paste Your Code Here'/>
-          <textarea id="message" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
+          <textarea id="message" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your code here..."></textarea>
           <br></br>
 
           <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Choose the programming language and it will read your code.</label>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import img from '../app/logo.png';
 import { Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from 'flowbite-react';
 
 export default function NavbarComponent() {
@@ -6,6 +7,7 @@ export default function NavbarComponent() {
         <div className='pt-2 pb-2 pl-4 pr-4'>
             <Navbar fluid rounded className='bg-blue-300 flex justify-center items-center'>
             <NavbarBrand as={Link} href='/'>
+                <img src={'images/logo.png'} alt='logo' className='w-14 h-14'/>
                 <span className='self-center whitespace-nowrap text-xl font-semibold dark:text-white'>SDP SOLUTIONS</span>
             </NavbarBrand>
             {/* <NavbarToggle/>
