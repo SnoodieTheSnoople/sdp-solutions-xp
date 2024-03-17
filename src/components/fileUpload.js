6cfb448bd0b6f865cfce5260a4e6525826d2da17
zuhzem <118192654+zuhzem@users.noreply.github.com>
2024-03-17 15:17:59 +0000

    Update fileUpload.js

--- src/components/fileUpload.js
+++ src/components/fileUpload.js
@@ -1,12 +1,29 @@
+
 import { FileInput, Label } from 'flowbite-react';
 
 export default function FileUploadComponent() {
+
+    const handleFileChange = (event) => {
+        const file = event.target.files[0]; 
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                setContent(e.target.result); 
+            };
+            reader.readAsText(file); 
+        }
+    };
+    
     return(
         <div>
             <div>
                 <Label htmlFor="file-upload-helper-text" value='Upload File'/>
             </div>
-            <FileInput id="file-upload-helper-text" helperText="SVG, PNG, JPG or GIF (MAX. 800x400px)." />
+            <FileInput 
+                id="file-upload-helper-text"
+                helperText="SVG, PNG, JPG or GIF (MAX. 800x400px)."
+                onChange={handleFileChange}
+            />
         </div>
     );
 }
