2277454f1a8e5f2a52805d887fc2d504ba523dc6
SerialCoder69 <162355785+SerialCoder69@users.noreply.github.com>
2024-03-22 04:45:35 +0000

    Update fileUpload.js
    ->Now will say results of calculation
    ->Only PHP and Java files can be uplaoded

--- src/components/fileUpload.js
+++ src/components/fileUpload.js
@@ -1,27 +1,27 @@
-
 import { FileInput, Label } from 'flowbite-react';
 
-export default function FileUploadComponent({ setContent }) {
+export default function FileUploadComponent({ onFileContent }) {
 
     const handleFileChange = (event) => {
-        const file = event.target.files[0]; 
+        const file = event.target.files[0];
         if (file) {
             const reader = new FileReader();
             reader.onload = (e) => {
-                setContent(e.target.result); 
+                onFileContent(e.target.result);
             };
-            reader.readAsText(file); 
+            reader.readAsText(file);
         }
     };
-    
-    return(
+
+    return (
         <div>
             <div>
                 <Label htmlFor="file-upload-helper-text" value='Upload File'/>
             </div>
-            <FileInput 
+            <FileInput
                 id="file-upload-helper-text"
-                helperText="SVG, PNG, JPG or GIF (MAX. 800x400px)."
+                helperText="Upload .PHP or .JAVA files."
+                accept=".php,.java" 
                 onChange={handleFileChange}
             />
         </div>
