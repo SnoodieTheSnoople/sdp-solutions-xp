ba300a3ac1601b65165640a3743ebc4dcbd5c191
Cleef <cleefgulfan312@gmail.com>
2024-03-23 19:35:47 +0000

    Added code from Zuhayr.

--- src/components/functions/test.js
+++ src/components/functions/test.js
@@ -1,1 +1,49 @@
-// This is a test file for the functions component. Only added to add the folder to Git.
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            complexity: 10,
+            efficiency: ''
+        };
+    }
+
+    componentDidMount() {
+
+        const efficiency = this.calculateCyclometricEfficiency(code);
+        this.setState({ efficiency });
+    }
+
+  calculateCyclometricEfficiency(complexity)
+  {
+    let efficiency = '';
+
+        if (complexity < 10){
+            efficiency === 'Simple procedure, little risk'
+        }
+        else if (11< complexity && complexity >20 )
+            { efficiency === 'More complex, moderate risk'
+        }
+        else if (21< complexity && complexity > 50)
+            { efficiency === 'Complex, high risk'
+        }
+        else
+            { efficiency === 'untestable code, very high risk'
+        }
+
+    return efficiency;
+
+  }
+
+
+    render() {
+        return (
+            <View>
+                <Text>Complexity efficiency {this.state.efficiency}</Text>
+
+            </View>
+        );
+    }
+}
